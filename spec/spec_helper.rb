937671045f8d61f0cb4ca0cdd2a05bddba1adfca
Lucy Borthwick <lucyborthwick95@gmail.com>
2017-10-17 15:44:00 +0100

    added database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 require 'capybara/rspec'
+require 'database_cleaner'
 # require 'simplecov'
 # require 'simplecov-console'
 require './app/models/link'
@@ -14,6 +15,7 @@ require './app/app'
 Capybara.app = BookmarkManager
 
 RSpec.configure do |config|
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
@@ -24,5 +26,18 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
 end
