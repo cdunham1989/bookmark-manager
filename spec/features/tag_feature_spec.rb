41eb5c106ff3df47f3d003235b33661954046ac2
Chris <cdunham1989@gmail.com>
2017-10-18 17:35:02 +0100

    allows you to input multiple tags, unit test passed

--- spec/features/tag_feature_spec.rb
+++ spec/features/tag_feature_spec.rb
@@ -3,10 +3,21 @@ feature 'tag adding' do
     visit '/links/new'
     fill_in :title, with: 'Link Title'
     fill_in :url, with: 'www.url.com'
-    fill_in :tag, with: 'new tag'
+    fill_in :tag, with: 'tag'
 
     click_button 'Add Bookmark'
     link = Link.first
-    expect(link.tags.map(&:name)).to include('new tag')
+    expect(link.tags.map(&:name)).to include('tag')
+  end
+
+  scenario 'I can add multiple tags to a new link' do
+    visit '/links/new'
+    fill_in :title, with: 'Link Title'
+    fill_in :url, with: 'www.url.com'
+    fill_in :tag, with: 'tag1 tag2'
+
+    click_button 'Add Bookmark'
+    link=Link.first
+    expect(link.tags.map(&:name)).to include('tag1', 'tag2')
   end
 end
